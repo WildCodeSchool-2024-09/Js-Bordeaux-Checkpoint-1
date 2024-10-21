e502120eaa98a299dcbee5c39c6d269c136246a2
Cahu07 <mathieu-cahuzac_student2024@wilder.school>
2024-10-21 10:09:27 +0200

    finish algo2 in part 1

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -7,8 +7,13 @@ Ex : 2004, 2016 et 2020 sont des années bissextiles
 Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utiliser l'opérateur "modulo" (%)
 */
 
-function isLeapYear(year) {
-  // Your code here !
+function isLeapYear(year){
+  let result;
+  if (year % 400){
+    result = "true" ;
+  } else {
+    result = "false";
+  }
 }
 
 module.exports = isLeapYear;
