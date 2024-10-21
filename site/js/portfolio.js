ded9b2d40df448090cb5f8d49f8d33d7a56f15f8
Ccilpy <cecilepuyol@gmail.com>
2024-10-21 12:22:48 +0200

    finish JS step 2 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,3 +5,17 @@ avatar.addEventListener("click", function () {
 });
 
 
+const buttonContainer = document.querySelector(".button-container");
+const firstNameButton = document.createElement("button");
+firstNameButton.classList.add("userNameButton");
+firstNameButton.textContent = "Modify text and color."
+buttonContainer.appendChild(firstNameButton);
+
+
+firstNameButton.addEventListener('click', function() {
+    let userInput = prompt("What's your name?");
+    let firstName = document.getElementById("firstname");
+    firstName.style.color="white";
+    firstName.replaceWith(userInput);
+})
+
