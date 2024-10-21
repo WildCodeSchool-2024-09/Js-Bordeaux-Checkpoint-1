525e21e6ba8565d02dbf27d377366d6c42a77b33
Ccilpy <cecilepuyol@gmail.com>
2024-10-21 12:40:40 +0200

    almost finsih JS step 3 in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -13,9 +13,12 @@ buttonContainer.appendChild(firstNameButton);
 
 
 firstNameButton.addEventListener('click', function() {
+    let userColorInput = prompt("Enter a color.");
+    let bgColor = document.querySelector(".description pink-bg");
     let userInput = prompt("What's your name?");
     let firstName = document.getElementById("firstname");
     firstName.style.color="white";
     firstName.replaceWith(userInput);
+    bgColor.style.backgroundColor=userColorInput;
 })
 
