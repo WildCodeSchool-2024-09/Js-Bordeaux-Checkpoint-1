bba53244521bcc3092ebb72f06e46c028709c57e
ThomasDev4 <tl.lemallier@gmail.com>
2024-10-21 12:31:47 +0200

    doesn't finish the js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,6 +2,13 @@ const imgAvatar = document.querySelector('.avatar');
 
 imgAvatar.addEventListener('click', function () {
   imgAvatar.src = "image/avatar.svg";
-  imgAvatar.alt = "Happy cute dog";
+  imgAvatar.alt = "";
 });
+
+const first = document.querySelector('#firstname')
+const button = document.querySelector('.description pink-bg')
+document.createElement('button')
+button.textContent = "modify text and color";
+button.classList.add('button')
+document.article.appendChild(button);
 
