3edda9c6465354c1dc219bdb4ab1aaeb2e24d5a6
Aurelien-sys <aurelienlaulan@icloud.com>
2024-10-21 12:25:42 +0200

    CCS étape 1 à 6, JS étape 1

---
+++ site/js/portfolio.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const avatarImage = document.getElementById('avatar');
+    avatarImage.addEventListener('click', function() {
+    avatarImage.src = 'image/avatar.svg';
+    });
+});
