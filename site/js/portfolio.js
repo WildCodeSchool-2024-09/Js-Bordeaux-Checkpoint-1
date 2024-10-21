0570253cb5c1ff15ca8f1ef3fc01da533fe33451
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-10-21 12:03:23 +0200

    finish part 1 of js, change avatar on click

---
+++ site/js/portfolio.js
@@ -0,0 +1,5 @@
+const avatar2 = document.querySelector(".mypic");
+avatar2.addEventListener("click", () => {
+	const image = document.querySelector(".mypic");
+	image.src = "./image/avatar-bis.png";
+});
