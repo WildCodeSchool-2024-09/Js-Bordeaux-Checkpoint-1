a5c5c605d3c572240762f3bae32b07696d8a0898
Raphaël <barre.raphael97@gmail.com>
2024-10-21 12:47:07 +0200

    Step 1 JS

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const avatar = document.getElementById('avatar');
-    const newAvatarSrc = 'image/logo-wcs.png';
+    const newAvatarSrc = 'image/avatar.svg';
 
     avatar.addEventListener('click', function() {
         avatar.src = newAvatarSrc;
