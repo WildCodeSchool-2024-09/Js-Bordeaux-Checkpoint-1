14735189895eaad40cbdcc331026ad499e7a2602
Makenchii <marwane422@hotmail.fr>
2024-10-21 11:48:08 +0200

    le js c'est chaud chaud

---
+++ site/js/portfolio.js
@@ -0,0 +1,9 @@
+const image = document.createElement("img")
+image.src = "avatar.svg"
+document.body.appendChild(image);
+const myButton = document.getElementById("myButton");
+
+
+myButton.addEventListener("click", function () {
+	myImg.src =avatar.svg";
+});
