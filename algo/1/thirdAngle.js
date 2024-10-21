bf11333df3def5177b249b9d10867c1acfd42f03
Aghiles-Belkalem <belkalemaghiles@gmail.com>
2024-10-21 09:46:12 +0200

    finish algo1 in part 1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,10 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+	return a - b;
+}
+function thirdAngle(a, b) {
+	return 180 - (a + b);
 }
 
 module.exports = thirdAngle;
