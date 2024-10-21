868cfbf90aa2a50933d8a4ed7bf119226fcd057d
ThomasDev4 <tl.lemallier@gmail.com>
2024-10-21 11:48:30 +0200

    i do my best on css and js

---
+++ site/js/portfolio.js
@@ -0,0 +1,7 @@
+const imgAvatar = document.querySelector('.avatar');
+
+imgAvatar.addEventListener('click', function () {
+  imgAvatar.src = "image/avatar.svg";
+  imgAvatar.alt = "Happy cute dog";
+});
+
