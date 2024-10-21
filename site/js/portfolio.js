3e88ee6420a68980bd97980af4eafccca05c7cd6
Sarah Smandack <sarah.smandack@gmail.com>
2024-10-21 10:27:39 +0200

    finish js step 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -2,3 +2,16 @@ const avatar = document.querySelector(".avatar");
 avatar.addEventListener('click', function(){
     avatar.src = 'image/avatar-bis.png'
 });
+
+const sectionPink = document.querySelector(".pink-bg");
+const nameButton = document.createElement('button');
+nameButton.type = 'button';
+nameButton.textContent = 'Change name and color';
+sectionPink.appendChild(nameButton);
+
+nameButton.addEventListener('click', function(){
+    let userName = prompt("What's your name ?");
+    const firstName = document.querySelector('span');
+    firstName.textContent = `${userName}`;
+    firstName.style.color = 'white';
+});
