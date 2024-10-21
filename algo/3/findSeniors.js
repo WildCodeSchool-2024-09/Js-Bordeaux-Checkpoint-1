01250a86d887dba2fe840c080d8406d344ff43ea
Geoffrey <geoffreyleglise@gmail.com>
2024-10-21 11:37:47 +0200

    I need Small groupe, I'm pumped

--- algo/3/findSeniors.js
+++ algo/3/findSeniors.js
@@ -26,7 +26,26 @@ const persons = [
 */
 
 function findSeniors(persons) {
-  // Your code here !
+  const persons = [
+    { name: 'Mary', experience: 2, job: 'web dev' },
+    { name: 'Tony', experience: 6, job: 'data analyst' },
+    { name: 'John', experience: 2, job: 'data analyst' },
+    { name: 'Jane', experience: 6, job: 'web dev' },
+    { name: 'Maggie', experience: 2, job: 'web dev' },
+    { name: 'Leonardo', experience: 2, job: 'data analyst' },
+    { name: 'Carla', experience: 4, job: 'data analyst' },
+    { name: 'Mickael', experience: 7, job: 'web dev' },
+    { name: 'Penelope', experience: 7, job: 'web dev' },
+    { name: 'Homer', experience: 5, job: 'data analyst' },
+    { name: 'Leonardo', experience: 2, job: 'data analyst' },
+    { name: 'Carla', experience: 4, job: 'web dev' },
+    { name: 'Lisa', experience: 3, job: 'web dev' },
+    { name: 'Millie', experience: 5, job: 'data analyst' },
+    { name: 'Penelope', experience: 7, job: 'web dev' },
+  ];
+  const tableauSenior = [];
+
+  
 }
 
 module.exports = findSeniors;
