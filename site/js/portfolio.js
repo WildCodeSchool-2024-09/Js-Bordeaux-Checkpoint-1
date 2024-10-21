7a4e1b4cef94b730aca197b952812d2b6e31befc
Wakoz <julien.cartron.pro@gmail.com>
2024-10-21 12:24:32 +0200

    update solution portefolio

---
+++ site/js/portfolio.js
@@ -0,0 +1,8 @@
+function changeImage(){
+    const avatar = document.getElementsByClassName(".avatar").src;
+    avatar.addEventListener("click", () => {
+        if (img.src == "image/avatar-bis.png"){
+            img.src = "image/avatar.svg";
+        }
+    } 
+)};
