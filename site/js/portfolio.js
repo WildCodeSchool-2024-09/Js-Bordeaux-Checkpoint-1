491bc567020892274819037f8c47e20c06d750ea
Yoann FORTIN <yoann.fortin.dev@gmail.com>
2024-10-21 15:22:58 +0200

    Second step on JS done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,5 +1,5 @@
+// First Step
 const avatar = document.querySelector("section:first-of-type > img");
-console.log(avatar);
 
 // avatar.addEventListener("click", function () {
 //   avatar.src = "image/avatar.svg";
@@ -7,3 +7,24 @@ console.log(avatar);
 avatar.addEventListener("click", () => {
    avatar.src = "image/avatar.svg";
 });
+
+// Second Step
+
+// 1 Cibler le container du parent
+const buttonContainer = document.querySelector(".description");
+//2 Créer le bouton
+const button = document.createElement("button");
+button.textContent = "Modify text and color";
+button.type = "button";
+// 3 Rattacher le bouton au parent
+buttonContainer.appendChild(button);
+// 4 Cibler le texte qui change
+const firstname = document.querySelector("#firstname");
+// 5 Écouter le click sur le bouton
+button.addEventListener("click", () => {
+   // 6 Stocker la valeur du prompt dans une variable
+   const value = prompt("Enter your name");
+   firstname.textContent = value;
+   firstname.style.color = "white";   
+})
+
